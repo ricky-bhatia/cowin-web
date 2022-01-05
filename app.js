610e89e51d6799e72bb5b8e80d16a939e394b141
Ricky Bhatia <rickybhatia11@gmail.com>
2022-01-05 17:26:55 +0530

    Update for 15-17 age group

--- app.js
+++ app.js
@@ -460,7 +460,7 @@
                     });
                 }
                 var span_class = (this.vaccine.toUpperCase()=='COVAXIN')?'bg-secondary':((this.vaccine.toUpperCase()=='COVISHIELD')?'bg-primary':'bg-dark');
-                var age_col = (this.allow_all_age==true)?'18 & Above':((this.min_age_limit==18)?'18-44 Only':((this.min_age_limit==45)?'45 & Above':'Unknown'));
+                var age_col = (this.allow_all_age==true)?'15 & Above':((this.min_age_limit==15)?'15-17':((this.min_age_limit==18)?'18 & Above':'Unknown'));
                 var book_btn= '<button type="button" class="btn btn-info btn-sm" onclick="manualBook(this)" data-centerid='+this.center_id+' data-sessionid='+this.session_id+' data-slot='+this.slots[this.slots.length - 1]+'>Book</button>';
                 var center = "<tr"+row_class+"><td>"+this.pincode+"</td><td>"+this.name+" <span class='badge rounded-pill "+span_class+"'>"+this.vaccine+"</span><BR/><span class='text-muted'><small>"+this.address+"</small><BR/><small class='text-primary'>"+ctimes+"</small></td><td>"+this.available_capacity_dose1+"</td><td>"+this.available_capacity_dose2+"</td><td>"+age_col+"</td><td>"+book_btn+"</td></tr>";
                 centerList.push(center);
